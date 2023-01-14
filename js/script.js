9b13ea592e0849f60d6c26cac5aeeeb165fbc47f
Mai <maimairupnik@gmail.com>
2023-01-14 17:01:53 +0100

    made the buttons show which one is selected

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let cdi = 0; // current display img
-let timeoutTime = 5000;
+let timeoutTime = 250;
 const bodyCons = document.getElementsByClassName("body-window");
 let sCon = 2;
 
@@ -11,7 +11,7 @@ window.addEventListener("load", () => {
   if (window.innerWidth <= 1000) swapHalfWidthImgLeft();
   else swapHalfWidthImgRight();
   resizeMainCanvas();
-  changeShownContainerWindow(true);
+  changeShownContainerWindowCustom(0);
 });
 
 window.addEventListener("resize", () => {
@@ -20,77 +20,6 @@ window.addEventListener("resize", () => {
   resizeMainCanvas();
   resizeBody();
 });
-
-function drawShape(ctx, xoff, yoff) {
-  ctx.beginPath();
-  ctx.moveTo(318 + xoff, 308 + yoff);
-  ctx.bezierCurveTo(
-    305 + xoff,
-    313 + yoff,
-    278 + xoff,
-    327 + yoff,
-    289 + xoff,
-    352 + yoff
-  );
-  ctx.bezierCurveTo(
-    295 + xoff,
-    365 + yoff,
-    316 + xoff,
-    365 + yoff,
-    332 + xoff,
-    362 + yoff
-  );
-  ctx.bezierCurveTo(
-    361 + xoff,
-    357 + yoff,
-    382 + xoff,
-    353 + yoff,
-    413 + xoff,
-    343 + yoff
-  );
-  ctx.bezierCurveTo(
-    446 + xoff,
-    332 + yoff,
-    446 + xoff,
-    331 + yoff,
-    452 + xoff,
-    328 + yoff
-  );
-  ctx.bezierCurveTo(
-    461 + xoff,
-    324 + yoff,
-    477 + xoff,
-    321 + yoff,
-    462 + xoff,
-    317 + yoff
-  );
-  ctx.bezierCurveTo(
-    448 + xoff,
-    313 + yoff,
-    449 + xoff,
-    318 + yoff,
-    420 + xoff,
-    326 + yoff
-  );
-  ctx.bezierCurveTo(
-    394 + xoff,
-    333 + yoff,
-    349 + xoff,
-    341 + yoff,
-    333 + xoff,
-    339 + yoff
-  );
-  ctx.bezierCurveTo(
-    313 + xoff,
-    336 + yoff,
-    317 + xoff,
-    321 + yoff,
-    318 + xoff,
-    312 + yoff
-  );
-  ctx.fillStyle = "rgba(252, 170, 38, 0.7)";
-  ctx.fill();
-}
 
 function swapHalfWidthImgLeft() {
   let tmp = document
@@ -154,10 +83,9 @@ function setDefault() {
     .classList.remove("header-button-selected");
   document
     .getElementById("menu-set-bezier-button")
-    .classList.remove("header-button-selected");
+    .classList.remove("header-button-selected");*/
   document.getElementById("main-body-display-selected-img-title").innerText =
     "Default";
-    */
 
   removeSelectedImgIcon();
   document
@@ -314,10 +242,97 @@ function changeShownContainerWindowCustom(setTo) {
   bodyCons[sCon].style.opacity = 0;
   bodyCons[setTo].style.opacity = 1;
   sCon = setTo;
+  selectButton();
   resizeBody();
 }
 
 function resizeBody() {
   document.getElementById("main-body").style.height =
     Math.floor(bodyCons[sCon].clientHeight + 120) + "px";
+}
+
+function unselectButtons() {
+  arr = document.getElementsByClassName("header-button");
+
+  for (i = 0; i < arr.length; i++)
+    arr[i].classList.remove("header-button-selected");
+}
+
+function selectButton() {
+  unselectButtons();
+
+  document
+    .getElementsByClassName("header-button")
+    [sCon].classList.add("header-button-selected");
+}
+
+function drawShape(ctx, xoff, yoff) {
+  ctx.beginPath();
+  ctx.moveTo(318 + xoff, 308 + yoff);
+  ctx.bezierCurveTo(
+    305 + xoff,
+    313 + yoff,
+    278 + xoff,
+    327 + yoff,
+    289 + xoff,
+    352 + yoff
+  );
+  ctx.bezierCurveTo(
+    295 + xoff,
+    365 + yoff,
+    316 + xoff,
+    365 + yoff,
+    332 + xoff,
+    362 + yoff
+  );
+  ctx.bezierCurveTo(
+    361 + xoff,
+    357 + yoff,
+    382 + xoff,
+    353 + yoff,
+    413 + xoff,
+    343 + yoff
+  );
+  ctx.bezierCurveTo(
+    446 + xoff,
+    332 + yoff,
+    446 + xoff,
+    331 + yoff,
+    452 + xoff,
+    328 + yoff
+  );
+  ctx.bezierCurveTo(
+    461 + xoff,
+    324 + yoff,
+    477 + xoff,
+    321 + yoff,
+    462 + xoff,
+    317 + yoff
+  );
+  ctx.bezierCurveTo(
+    448 + xoff,
+    313 + yoff,
+    449 + xoff,
+    318 + yoff,
+    420 + xoff,
+    326 + yoff
+  );
+  ctx.bezierCurveTo(
+    394 + xoff,
+    333 + yoff,
+    349 + xoff,
+    341 + yoff,
+    333 + xoff,
+    339 + yoff
+  );
+  ctx.bezierCurveTo(
+    313 + xoff,
+    336 + yoff,
+    317 + xoff,
+    321 + yoff,
+    318 + xoff,
+    312 + yoff
+  );
+  ctx.fillStyle = "rgba(252, 170, 38, 0.7)";
+  ctx.fill();
 }
